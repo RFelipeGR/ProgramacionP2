21aaa17471ed2cef48407bfb13e3d8fb4357fd60
laboratorio <laboratorio@UPOAULA10316>
2024-12-03 07:28:15 -0500

    Agregar archivos de proyecto.

---
+++ GuañaRomeroRobertoFelipe/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GuañaRomeroRobertoFelipe
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GuañaRomeroRobertoFelipe/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GuañaRomeroRobertoFelipe
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
